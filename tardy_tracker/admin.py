692e8ac2fb3fb3f40819238bbe7827bcb278eac8
Adam <adam.grossman08@me.com>
2014-10-17 14:08:30 -0700

    delete class

--- tardy_tracker/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from tardy_tracker.models import User, Course, CheckIn
-
-
-admin.site.register(User)
-admin.site.register(Course)
-admin.site.register(CheckIn)
